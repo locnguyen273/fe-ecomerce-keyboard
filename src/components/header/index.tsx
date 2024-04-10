b791346363ea09fe9c17e13cbd5e1815e993218d
Loc Nguyen <nguyentanloc@teams.airlab.dev>
2024-04-10 18:22:38 +0700

    [update] add custom header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,39 +1,44 @@
-import { NavLink } from "react-router-dom";
-import { Input, Image, Button } from 'antd';
-import type { SearchProps } from 'antd/es/input/Search';
-import Logo from "../../assets/images/logo-desktop.jpg"
+
+import { Input, Image, Button } from "antd";
+import type { SearchProps } from "antd/es/input/Search";
+import Logo from "../../assets/images/logo-desktop.png";
+import { Link } from "react-router-dom";
 
 const { Search } = Input;
 
 const Header = () => {
-  const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
+  const onSearch: SearchProps["onSearch"] = (value, _e, info) =>
+    console.log(info?.source, value);
 
   return (
     <div className="header">
       <div className="header__container">
-        <div className="header__row">
-          <p className="text-white">Miễn phí vận chuyển cho đơn hàng trên 1.000.000 VNĐ</p>
-          <p className="text-white">Hotline: <NavLink className="text-white" to={""}>0987654321</NavLink></p>
-        </div>
         <div className="header__row header__center">
-          <Image
-            className="header__center--logo"
-            src={Logo}
-            preview={false}
+          <Image className="header__center--logo" src={Logo} preview={false} />
+          <div className="header__menu-list">
+            <Link to={""} className="header__menu-list--item">trang chủ</Link>
+            <Link to={""} className="header__menu-list--item">sản phẩm</Link>
+            <Link to={""} className="header__menu-list--item">tin tức</Link>
+            <Link to={""} className="header__menu-list--item">liên hệ</Link>
+          </div>
+          <Search
+            className="header__search"
+            placeholder="Tìm kiếm sản phẩm..."
+            allowClear
+            onSearch={onSearch}
           />
-          <Search className="header__center--search" placeholder="Tìm kiếm sản phẩm..." allowClear onSearch={onSearch} />
-          <div className="header__center--right">
-            <Button className="header__center--btn-user">
+          <div className="header__right">
+            <Button className="header__right--btn-user">
               <i className="fa-solid fa-user"></i>
             </Button>
-            <Button className="header__center--btn-bag">
+            <Button className="header__right--btn-bag">
               <i className="fa-solid fa-basket-shopping"></i>
             </Button>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
