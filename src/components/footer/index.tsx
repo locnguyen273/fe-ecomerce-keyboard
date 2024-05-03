01ba79e62625f8773ef480b5a37523738e5a765e
locnguyen273 <77155886+locnguyen273@users.noreply.github.com>
2024-05-03 10:18:37 +0700

    Sprint 1 home page (#2)
    * [update] add custom header
    
    * [update] add modal popup
    
    * [update] add component and style modal search - carousel
    
    * [update] add component category and custom style app
    
    * [update] add ui new product page
    
    * [update] add style and component
    
    * [update] custom style modal search
    
    * .
    
    * .
    
    * [update] end sprint 1 home page
    
    ---------
    
    Co-authored-by: Loc Nguyen <nguyentanloc@teams.airlab.dev>

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,65 @@
-
+import { Image, Input } from "antd";
+import Logo from "../../assets/images/logo-desktop.png";
+import type { SearchProps } from "antd/es/input/Search";
+import { Link } from "react-router-dom";
+const { Search } = Input;
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const onSearch: SearchProps["onSearch"] = (value, _e, info) =>
+    console.log(info?.source, value);
 
-export default Footer
+  return (
+    <div className="footer">
+      <div className="footer__container">
+        <div className="footer__column">
+          <div className="footer__column--logo">
+            <Image src={Logo} alt="" preview={false} />
+          </div>
+          <div className="footer__column__list">
+            <p className="footer__column--content">Địa chỉ: 54/3, ĐHT09, Phường Đông Hưng Thuận, Q12, TP HCM</p>
+            <p className="footer__column--content">Điện thoại: 0987654321</p>
+            <p className="footer__column--content">Email: tanlocit20062000@gmail.com</p>
+          </div>
+        </div>
+        <div className="footer__column">
+          <h5 className="footer__column--title">chính sách và liên kết</h5>
+          <div className="footer__column__list">
+            <Link to={"/"} className="footer__column--content">Chính sách vận chuyển</Link>
+            <Link to={"/"} className="footer__column--content">Chính sách bảo hành</Link>
+            <Link to={"/"} className="footer__column--content">Chính sách bảo mật</Link>
+            <Link to={"/"} className="footer__column--content">Chính sách đổi trả</Link>
+          </div>
+        </div>
+        <div className="footer__column box-three">
+          <h5 className="footer__column--title">nhận tin khuyến mãi</h5>
+          <Search
+            className="footer__column--search"
+            placeholder="Nhập email của bạn"
+            enterButton="Đăng ký"
+            size="middle"
+            onSearch={onSearch}
+          />
+          <div className="footer__column__social-list">
+            <Link to={"/"} className="footer__column__social-list--item facebook">
+              <i className="fa-brands fa-facebook-f"></i>
+            </Link>
+            <Link to={"/"} className="footer__column__social-list--item instagram">
+            <i className="fa-brands fa-square-instagram"></i>
+            </Link>
+            <Link to={"/"} className="footer__column__social-list--item youtube">
+              <i className="fa-brands fa-youtube"></i>
+            </Link>
+            <Link to={"/"} className="footer__column__social-list--item tiktok">
+              <i className="fa-brands fa-tiktok"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <p className="footer__bottom">
+        © Bản quyền thuộc về centerkeyboardsgop.com.vn | Thiết kế và duy trì bởi Center Keyboard Shop
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
